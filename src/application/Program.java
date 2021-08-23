848238eff99d29115ce45a1525610144695b850a
ylmoura <yvesluan016@gmail.com>
2021-08-23 16:25:25 -0300

    Projeto Criado

---
+++ src/application/Program.java
@@ -0,0 +1,54 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		Product product = new Product();
+		System.out.println("Enter product data: ");
+		System.out.println("Name: ");
+		product.name = sc.nextLine();
+		System.out.print("Price:");
+		product.price = sc.nextDouble();
+		System.out.print("Quantity in stock: ");
+		product.quantity = sc.nextInt();
+		
+		System.out.println();
+		System.out.println("Product data: " + product);
+		
+		System.out.println();
+		System.out.println("Enter the number of products to be adden in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Update data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Update data: " + product);
+		
+		
+		
+		//System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+		
+		
+		sc.close();
+
+	}
+
+}
